2a8445d79b441b9fe4dc38e53b38a31fd8391fbb
EmreYYZ <6238977+EmreYYZ@users.noreply.github.com>
2021-08-08 11:36:16 -0400

    Added links to Footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -3,19 +3,20 @@ import { Legend } from "./Legend";
 
 export const Footer = () => {
   return (
-    <div>
+    <div className="my-10">
+      <Legend />
+
       <p className="text-lg">
         Made by{" "}
-        <a href="https://emre.la" target="_blank" rel="noopener noreferrer">
+        <a className="font-extrabold" href="https://emre.la" target="_blank" rel="noopener noreferrer">
           Emre
         </a>
-        . Code is on{" "}
-        <a href="https://github.com/EmreYYZ/HN-Paywall-Archiver" target="_blank" rel="noopener noreferrer">
+        . The code is on{" "}
+        <a className="font-extrabold" href="https://github.com/EmreYYZ/HN-Paywall-Archiver" target="_blank" rel="noopener noreferrer">
           GitHub
         </a>
         .
       </p>
-      <Legend />
     </div>
   );
 };
