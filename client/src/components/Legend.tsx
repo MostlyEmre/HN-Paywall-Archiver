f5d4ad91d2ba1f64dac18850b8b57ca08b4a596e
EmreYYZ <6238977+EmreYYZ@users.noreply.github.com>
2021-08-08 11:36:37 -0400

    Better Legend design/layout

--- client/src/components/Legend.tsx
+++ client/src/components/Legend.tsx
@@ -2,22 +2,22 @@ import React from "react";
 
 export const Legend = () => {
   return (
-    <div className="shadow text-lg text-gray-500 px-4 pt-4 pb-2 bg-gray-50 rounded-xl m-4 inline-block">
-      <p className="mx-2 mb-2 font-extrabold text-3xl text-gray-400">Legend</p>
-      <div className="m-2 inline-block">
-        <div className="bg-yellow-500 inline-block w-4 h-4 rounded-full"></div>
-
-        <p className="inline-block ml-2">Archiving</p>
+    <div className="border-4 border-dashed border-gray-500  rounded-xl p-4 inline-block">
+      <h3 className="text-2xl font-extrabold mb-2">Legend</h3>
+      <div className="inline-block mr-4">
+        <p className="inline-block">
+          <span className="bg-yellow-500 inline-block w-3 h-3 rounded-full"></span> Archiving
+        </p>
       </div>
-      <div className="m-2 inline-block">
-        <div className="bg-green-500 inline-block w-4 h-4 rounded-full"></div>
-
-        <p className="inline-block ml-2">Archived</p>
+      <div className="inline-block mr-4">
+        <p className="inline-block ">
+          <span className="bg-green-500 inline-block w-3 h-3 rounded-full"></span> Archived
+        </p>
       </div>
-      <div className="m-2 inline-block">
-        <div className="bg-blue-500 inline-block w-4 h-4 rounded-full"></div>
-
-        <p className="inline-block ml-2">Already Archived</p>
+      <div className="inline-block mr-4">
+        <p className="inline-block ">
+          <span className="bg-blue-500 inline-block w-3 h-3 rounded-full"></span> Already Archived
+        </p>
       </div>
     </div>
   );
